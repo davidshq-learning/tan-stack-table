377c5496923626c3ef2e2e990f342dea26883a2f
Dave Mackey <dave@davemackey.net>
2024-05-05 16:26:26 -0400

    Make code more concise

--- frontend-nextjs/api/useGetUsers.ts
+++ frontend-nextjs/api/useGetUsers.ts
@@ -3,25 +3,19 @@ import { UseUsersInput, UseUsersResponse } from "../types/Users";
 
 const backend_url = "http://localhost:3000";
 
-const getAllUsersFn: {
-  ({
-    sorting,
-    columnFilters,
-    pagination,
-  }: UseUsersInput): Promise<UseUsersResponse>;
-} = async ({ sorting, columnFilters, pagination }: UseUsersInput) => {
-  // set pagination
+const getAllUsersFn = async ({ sorting, columnFilters, pagination }: UseUsersInput): Promise<UseUsersResponse> => {
+  // Define the values for pagination, filtering, and sorting
   const page = pagination.pageIndex + 1,
     per_page = pagination.pageSize;
-  // set filtering
   const filters = columnFilters.reduce((acc, { id, value }) => ({ ...acc, [id]: value }), {});
-  // set sorting
   const sorting_param = sorting.map(({ id, desc }) => `${id}:${desc ? 'desc' : 'asc'}`).join(',');
 
+  // Create the URL with the query parameters
   const url = new URL(`${backend_url}/users`);
   const params = { ...filters, sortBy: sorting_param, page, limit: per_page };
   Object.entries(params).forEach(([key, value]) => url.searchParams.append(key, String(value)));
 
+  // Attempt to fetch URL
   let res;
   try {
     res = await fetch(url.toString());
@@ -36,14 +30,10 @@ const getAllUsersFn: {
   return await res.json();
 };
 
-export const useGetUsers = ({
-  sorting,
-  columnFilters,
-  pagination,
-}: UseUsersInput) => {
+export const useGetUsers = (input: UseUsersInput) => {
   const { data: allUsersData, error } = useSWR<UseUsersResponse, Error>(
-    ["users", sorting, columnFilters, pagination],
-    () => getAllUsersFn({ sorting, columnFilters, pagination })
+    ["users", input.sorting, input.columnFilters, input.pagination],
+    () => getAllUsersFn(input)
   );
 
   const isAllUsersDataLoading = !allUsersData && !error;
