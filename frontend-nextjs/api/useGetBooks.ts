f178df921b16efc0d2c6213d799a34f5f6ecac9c
Dave Mackey <dave@davemackey.net>
2024-05-05 22:42:14 -0400

    Add env vars and auth to useGetBooks; changing user to books

--- frontend-nextjs/api/useGetBooks.ts
+++ frontend-nextjs/api/useGetBooks.ts
@@ -1,42 +1,63 @@
 import useSWR from 'swr';
-import { UseBooksInput, UseBooksResponse } from "../types/Books";
+import {UseBooksInput, UseBooksResponse} from "@/types/Books";
 
-const backend_url = "http://localhost:3000";
+const search_url = process.env.NEXT_PUBLIC_OSS_HOST;
+const username = process.env.NEXT_PUBLIC_OSS_USERNAME;
+const password = process.env.NEXT_PUBLIC_OSS_PASSWORD;
+console.log('username', username)
 
-const getAllUsersFn = async ({ sorting, columnFilters, pagination }: UseBooksInput): Promise<UseBooksResponse> => {
-  // Define the values for pagination, filtering, and sorting
-  const page = pagination.pageIndex + 1,
-    per_page = pagination.pageSize;
-  const filters = columnFilters.reduce((acc, { id, value }) => ({ ...acc, [id]: value }), {});
-  const sorting_param = sorting.map(({ id, desc }) => `${id}:${desc ? 'desc' : 'asc'}`).join(',');
+const getAllBooksFn = async ({ sorting, columnFilters, pagination }: UseBooksInput): Promise<UseBooksResponse> => {
+    // Define the values for pagination, filtering, and sorting
+    const page = pagination.pageIndex + 1,
+        per_page = pagination.pageSize;
 
-  // Create the URL with the query parameters
-  const url = new URL(`${backend_url}/users`);
-  const params = { ...filters, sortBy: sorting_param, page, limit: per_page };
-  Object.entries(params).forEach(([key, value]) => url.searchParams.append(key, String(value)));
+    const filters = columnFilters.reduce((acc, { id, value }) => ({ ...acc, [id]: value }), {});
+    const sorting_param = sorting.map(({ id, desc }) => `${id}:${desc ? 'desc' : 'asc'}`).join(',');
 
-  // Attempt to fetch URL
-  let res;
-  try {
-    res = await fetch(url.toString());
-  } catch (err) {
-    throw new Error("Network error");
-  }
+    // Create the URL with the query parameters
+    const url = new URL(`${search_url}`);
 
-  if (!res.ok) {
-    throw new Error(res.statusText);
-  }
+    // Encode the username and password
+    const auth = `${username}:${password}`
+    const encodedAuth = window.btoa(auth)
 
-  return await res.json();
+    // Add authorization header to call
+    const headers = new Headers({
+        'Authorization': `Basic ${encodedAuth}`,
+    })
+
+    const params = { ...filters, sort: sorting_param, from: page, size: per_page };
+    Object.entries(params).forEach(([key, value]) => url.searchParams.append(key, String(value)));
+
+    // Attempt to fetch URL
+    let res;
+    try {
+        res = await fetch(url.toString(), {headers});
+    } catch (err) {
+        throw new Error("Network error");
+    }
+
+    if (!res.ok) {
+        throw new Error(res.statusText);
+    }
+
+    const response = await res.json()
+    
+    console.log('response', response)
+
+    const books = response.hits.hits.map(hit => hit._source);
+
+    console.log('books', books)
+    return books;
 };
 
-export const useGetUsers = (input: UseBooksInput) => {
-  const { data: allUsersData, error } = useSWR<UseBooksResponse, Error>(
-    ["users", input.sorting, input.columnFilters, input.pagination],
-    () => getAllUsersFn(input)
-  );
+export const useGetBooks = (input: UseBooksInput) => {
+    const { data: allBooksData, error } = useSWR<UseBooksResponse, Error>(
+        ["books", input.sorting, input.columnFilters, input.pagination],
+        () => getAllBooksFn(input)
+    );
 
-  const isAllUsersDataLoading = !allUsersData && !error;
+    const isAllBooksDataLoading = !allBooksData && !error;
 
-  return { allUsersData, isAllUsersDataLoading, error };
+    return {allBooksData, isAllBooksDataLoading, error};
 };
